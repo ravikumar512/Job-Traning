8003f743205a673628f6be3510e52e9f6770e300
ravikumar512 <ravi96151234@gmail.com>
2024-08-05 04:49:17 -0600

    Create Sum of two number.js

---
+++ ProblemSolving/Sum of two number.js
@@ -0,0 +1,8 @@
+/*  Create two variables `a` and `b`, assign them the values 5 and 10 respectively. Calculate their sum and print the result.
+   - Help: Use the `+` operator to add numbers. */
+
+let a,b;
+a = 5;
+b = 10;
+let sum = a + b;
+console.log(sum);
