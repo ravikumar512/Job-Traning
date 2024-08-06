7ad72d5b4a638dc280f97813dd8f8159ca3f9e4f
ravikumar512 <ravi96151234@gmail.com>
2024-08-06 04:11:42 -0600

    Create combined  String.js

---
+++ ProblemSolving/combined  String.js
@@ -0,0 +1,10 @@
+/*  Declare a variable `greeting` and assign it the value `Hello`. 
+Declare another variable `name` and assign it your name. 
+Concatenate these two variables and print the result.
+   - Help: Use the `+` operator to concatenate strings.*/
+
+
+let gretting = "Hello"
+fullname = "Ravi Kumar";
+let concatenate ="Hello "+ "Ravi Kumar"
+console.log(concatenate);
