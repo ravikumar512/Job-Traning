3c2202a69a0139a4cd8eb6b7c2aa191ff5e7d4f4
ravikumar512 <ravi96151234@gmail.com>
2024-08-06 03:49:50 -0600

    Create temperature.js

---
+++ ProblemSolving/temperature.js
@@ -0,0 +1,6 @@
+/*  Declare a variable `temperature` and assign it the value 72. Convert it to Celsius and print the result.
+   - Formula: Celsius = (Fahrenheit - 32)  5/9    */
+
+let temeracture = 72;
+let calsius = ( temeracture - 32)* 5/9;
+console.log(calsius);
