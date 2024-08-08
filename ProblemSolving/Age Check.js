d190a97537da551f3f96a0bd0df2cff41b9fadff
ravikumar512 <ravi96151234@gmail.com>
2024-08-08 06:08:17 -0600

    Create Age Check.js

---
+++ ProblemSolving/Age Check.js
@@ -0,0 +1,13 @@
+/*write an if-else statement to check if a variable 'age' is greater then or equal
+to 18. print "adult" if true, otherwise print "minor". */
+
+let age = prompt("Enter Your Age");
+
+            if (age >= 18) 
+    {
+        console.log ("Adult");
+    }
+            else
+    {
+         console.log ("Minor");
+    }
