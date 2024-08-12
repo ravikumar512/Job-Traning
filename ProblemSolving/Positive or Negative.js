cbfdce8926dc9bd027d74cede5aa574d81f41773
ravikumar512 <ravi96151234@gmail.com>
2024-08-12 06:16:25 -0600

    Create Positive or Negative.js

---
+++ ProblemSolving/Positive or Negative.js
@@ -0,0 +1,12 @@
+/* Write a program that checks if a variable 'num' is positive, negavtie, or zero.
+  Print the appriate message. */
+
+let num =  (prompt("enter a number"));
+
+ if (num > 0) {
+    console.log("The number is positive");
+ }else if (num < 0){
+    console.log("The number is neative");
+ }else{
+    console.log("The nummber is zero");
+ }
