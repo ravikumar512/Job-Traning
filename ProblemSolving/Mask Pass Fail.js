1f3280b1b07a3597d8abf92a016b3c2104bbbccf
ravikumar512 <ravi96151234@gmail.com>
2024-08-12 06:11:06 -0600

    Create Mask Pass Fail.js

---
+++ ProblemSolving/Mask Pass Fail.js
@@ -0,0 +1,11 @@
+/* Write a prgrame that check if a variable 'score' is between 50 and 100
+(inculsive). Print "Pass" if true, otherwhise print "Fail". */
+
+let score = 5;
+ 
+if (score>=50 && score <= 100)
+    {
+   console.log("pass");
+ } else {
+    console.log("fail");
+ }
